de1cc2072f187aa62f129a2a2ad3686d40bc2f77
maha <vipmha@hotmail.com>
2023-10-08 16:08:24 +0300

    Initial Commit

---
+++ try11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  try11
+//
+//  Created by maha on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ try11/try11App.swift
@@ -0,0 +1,17 @@
+//
+//  try11App.swift
+//  try11
+//
+//  Created by maha on 23/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct try11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
